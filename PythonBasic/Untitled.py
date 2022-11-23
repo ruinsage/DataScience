a59a6bfd552343ecfb6286c102eafcfaad595e70
tj <runmage@hanmail.net>
2022-11-23 18:21:17 +0900

    11월 23일 마지막 작업

---
+++ PythonBasic/Untitled.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+print("Jupyter 테스트1")
+
+
+# In[22]:
+
+
+# A키는 현재 셀의 위에 새로운 셀을 삽입한다.
+print('프로그램 실행 준비')
+
+num1 = int(input("첫 번째 수를 입력하세요"))
+# B키는 현재 셀의 아래에 새로운 셀을 삽입한다.
+
+
+# In[23]:
+
+
+num1
+
+
+# In[24]:
+
+
+type(num1)
+
+
+# In[25]:
+
+
+num2 = int(input("두 번째 수를 입력하세요"))
+
+
+# In[26]:
+
+
+num2
+
+
+# In[27]:
+
+
+num1+num2
+
+
+# In[20]:
+
+
+print("Jupyter 테스트1")
+# A키는 현재 셀의 위에 새로운 셀을 삽입한다.
+print('프로그램 실행 준비')
+
+num1 = int(input("첫 번째 수를 입력하세요"))
+# B키는 현재 셀의 아래에 새로운 셀을 삽입한다.
+num2 = int(input("두 번째 수를 입력하세요"))
+num1+num2
+
