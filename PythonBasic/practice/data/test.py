85e2753757373438efb0ee7b76464b5c6e1aff6a
pimbee <pimbeelove@naver.com>
2022-12-01 18:21:56 +0900

    11월 30일 마지막 작업2

--- PythonBasic/practice/data/test.py
+++ PythonBasic/practice/data/test.py
@@ -1,9 +1,9 @@
-weight_loss_n = [73,72,72,71,72,71,70,69,68,67,67,66,66,67,66,65]
+index_list = []
+def make_index(n):
+    for month in range(1,n+1):
+        for week in range(1,n+1):
+            index = str(month) + '월' + str(week) + '주'
+            index_list.append(index)
 
-loss_list = []
-for i in range(0,15):
-    loss = weight_loss_n[i] - weight_loss_n[i+1]
-    loss_list.append(loss)
-
-print(loss_list)
-
+make_index(4)
+print(index_list)
