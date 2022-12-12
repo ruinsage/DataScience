7b3abc2fa4a1d207b6e22e26c4ae0ff0bffc7311
pimbee <pimbeelove@naver.com>
2022-12-12 18:23:42 +0900

    12월 9일 마지막 작업 Initial commit

--- 02_ETL/03_Data_Format/2. Excel/13pandas_excel_concat_data_from_multiple_workbooks.py
+++ 02_ETL/03_Data_Format/2. Excel/13pandas_excel_concat_data_from_multiple_workbooks.py
@@ -13,8 +13,8 @@ for data in input_files:
     for name, dataframe in files.items():
         Workbook.append(dataframe)
         #print(dataframe)
-all_dataframe = pd.concat(Workbook, axis= 0, ignore_index= True)
-#print(all_dataframe)
+#all_dataframe = pd.concat(Workbook, axis= 0, ignore_index= True)
+print(Workbook)
 #writer = pd.ExcelWriter(output_file)
 #all_dataframe.to_excel(writer, sheet_name='all_data_all_workbooks', index=False)
 #writer.save()
