28ae58fca5ae92e22ea55a179f1370b6451f44e8
pimbee <pimbeelove@naver.com>
2022-12-16 18:19:57 +0900

    12월 16일 마지막 작업

---
+++ 02_ETL/03_Data_Format/5. JSON/sample_format.py
@@ -0,0 +1,5 @@
+{
+	"response": {
+		"body":  {
+			"student_list": [ {
+			"age": 45, "major": "경영학", "name": "영호","sex": "남" }, { "age": 30, "major": "정치외교", "name": "세중", "sex": "남" }, {    "age": 30, "major": "신문방송학", "name": "치우",  "sex": "남"  }  ]        }    }}
