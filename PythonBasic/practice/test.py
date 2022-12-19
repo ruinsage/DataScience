323735779ca04052252ee19dab1a5c41087d51b4
pimbee <pimbeelove@naver.com>
2022-12-19 18:20:42 +0900

    12월 19일 작업

--- PythonBasic/practice/test.py
+++ PythonBasic/practice/test.py
@@ -1,3 +1,13 @@
+def solution(slice, n):
+    answer = 0
+    if n >= 1 and n <= 100 and slice >=2 and slice <=10:
+        if n % slice == 0:
+            answer = n // slice
+        else:
+            answer = n // slice + 1
+    return answer
 
-
-
+slice = 7
+n = 16
+print(solution(slice, n))
+print(((n - 1) // slice) + 1)
