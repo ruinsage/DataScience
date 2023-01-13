2d2a991b523357909762e4691fd50fd9c03f11a0
pimbee <pimbeelove@naver.com>
2023-01-13 18:29:33 +0900

    Merge branch 'master' of https://github.com/ruinsage/DataScience
    # Conflicts:
    #	03_RPA/2차프로젝트/test.py

--- 03_RPA/2차프로젝트/herricane.py
+++ 03_RPA/2차프로젝트/herricane.py
@@ -4,9 +4,9 @@ import time
 import cx_Oracle
 import pandas as pd
 
-def make_RoadWork_df() :
+def make_herricane_df(shhet_name) :
     input_file = 'herricane.xlsx'
-    RoadWork_df = pd.read_excel(input_file, sheet_name='RoadWork')
+    RoadWork_df = pd.read_excel(input_file, sheet_name=shhet_name)
     print(RoadWork_df)
     return RoadWork_df
 
@@ -39,12 +39,6 @@ def hericane_RoadWork_to_oracle(RoadWork) :
     cur.close()
     con.close()
 
-def make_ROADEVENT_df() :
-    input_file = 'herricane.xlsx'
-    ROADEVENT_df = pd.read_excel(input_file, sheet_name='ROADEVENT')
-    print(ROADEVENT_df)
-    return ROADEVENT_df
-
 def hericane_ROADEVENT_to_oracle(ROADEVENT_df) :
     current_time = time.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -72,12 +66,6 @@ def hericane_ROADEVENT_to_oracle(ROADEVENT_df) :
     cur.close()
     con.close()
 
-def make_RoadCondition_df() :
-    input_file = 'herricane.xlsx'
-    RoadCondition_df = pd.read_excel(input_file, sheet_name='RoadCondition')
-    print(RoadCondition_df)
-    return RoadCondition_df
-
 def hericane_RoadCondition_to_oracle(RoadCondition_df) :
     current_time = time.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -104,12 +92,6 @@ def hericane_RoadCondition_to_oracle(RoadCondition_df) :
     cur.close()
     con.close()
 
-def make_RoadClose_df() :
-    input_file = 'herricane.xlsx'
-    RoadClose_df = pd.read_excel(input_file, sheet_name='RoadClose')
-    print(RoadClose_df)
-    return RoadClose_df
-
 def hericane_RoadClose_to_oracle(RoadClose_df) :
     current_time = time.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -134,6 +116,124 @@ def hericane_RoadClose_to_oracle(RoadClose_df) :
         text = RoadClose_df.iloc[i]['text']
 
         cur.execute(query, (ocrr_id, current_time, ocrr_id, lat, lon, heading, link_id, whole_lane, block_lane, text, current_time, current_time))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+def make_sunny_df(sheet_name) :
+    input_file = 'sunny.xlsx'
+    df = pd.read_excel(input_file, sheet_name=sheet_name)
+    print(df)
+    return df
+
+def sunny_RoadWork_to_oracle(RoadWork) :
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    con = cx_Oracle.connect('open_source/1111@localhost:1521/xe')
+    cur = con.cursor()
+    query = 'merge into ROAD_WORK ' \
+            'using DUAL on (id = :ocrr_id) ' \
+            'when matched then ' \
+            'update set last_updated_at = :current_time ' \
+            'when not matched then ' \
+            'insert (id, lat, lon, heading, link_id, whole_lane, block_lane, text, created_at, last_updated_at) ' \
+            'values (:ocrr_id, :lat, :lon, :heading, :link_id, :whole_lane, :block_lane, :text, :current_time, :current_time)'
+
+    for i in range(len(RoadWork)):
+        ocrr_id = int(RoadWork.iloc[i]['ocrr_id'])
+        lat = int(RoadWork.iloc[i]['latitude'])  # int 값에 대해서는 int 형으로 변환해줘야 한다.
+        lon = int(RoadWork.iloc[i]['longitude'])
+        heading = int(RoadWork.iloc[i]['heading'])
+        link_id = int(RoadWork.iloc[i]['link_id'])
+        whole_lane = int(RoadWork.iloc[i]['whole_lane'])
+        block_lane = int(RoadWork.iloc[i]['block_lane'])
+        text = RoadWork.iloc[i]['text']
+
+        cur.execute(query, (ocrr_id, current_time, ocrr_id, lat, lon, heading, link_id, whole_lane, block_lane, text, current_time, current_time))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+
+def sunny_ROADEVENT_to_oracle(ROADEVENT_df) :
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    con = cx_Oracle.connect('open_source/1111@localhost:1521/xe')
+    cur = con.cursor()
+    query = 'merge into ROAD_EVENT ' \
+            'using DUAL on (id = :ocrr_id) ' \
+            'when matched then ' \
+            'update set last_updated_at = :current_time ' \
+            'when not matched then ' \
+            'insert (id, lat, lon, heading, link_id, code, created_at, last_updated_at) ' \
+            'values (:ocrr_id, :lat, :lon, :heading, :link_id, :code, :current_time, :current_time)'
+
+    for i in range(len(ROADEVENT_df)):
+        ocrr_id = ROADEVENT_df.iloc[i]["ocrr_id"]
+        lat = int(ROADEVENT_df.iloc[i]["latitude"])
+        lon = int(ROADEVENT_df.iloc[i]["longitude"])
+        heading = int(ROADEVENT_df.iloc[i]["heading"])
+        link_id = int(ROADEVENT_df.iloc[i]["link_id"])
+        code = int(ROADEVENT_df.iloc[i]["code"])
+
+        cur.execute(query, (ocrr_id, current_time, ocrr_id, lat, lon, heading, link_id, code, current_time, current_time))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+def sunny_RoadClose_to_oracl(RoadClose_df):
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    con = cx_Oracle.connect('open_source/1111@localhost:1521/xe')
+    cur = con.cursor()
+    query = 'merge into ROAD_CLOSE ' \
+            'using DUAL on (id = :ocrr_id) ' \
+            'when matched then ' \
+            'update set last_updated_at = :current_time ' \
+            'when not matched then ' \
+            'insert (id, lat, lon, heading, link_id, whole_lane, block_lane, text, created_at, last_updated_at) ' \
+            'values (:ocrr_id, :lat, :lon, :heading, :link_id, :whole_lane, :block_lane, :text, :current_time, :current_time)'
+
+    for i in range(len(RoadClose_df)):
+        ocrr_id = int(RoadClose_df.iloc[i]['ocrr_id'])
+        lat = int(RoadClose_df.iloc[i]['latitude'])  # int 값에 대해서는 int 형으로 변환해줘야 한다.
+        lon = int(RoadClose_df.iloc[i]['longitude'])
+        heading = int(RoadClose_df.iloc[i]['heading'])
+        link_id = int(RoadClose_df.iloc[i]['link_id'])
+        whole_lane = int(RoadClose_df.iloc[i]['whole_lane'])
+        block_lane = int(RoadClose_df.iloc[i]['block_lane'])
+        text = RoadClose_df.iloc[i]['text']
+
+        cur.execute(query, (ocrr_id, current_time, ocrr_id, lat, lon, heading, link_id, whole_lane, block_lane, text, current_time, current_time))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+def sunny_RoadCondition_to_oracle(RoadCondition_df) :
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    con = cx_Oracle.connect('open_source/1111@localhost:1521/xe')
+    cur = con.cursor()
+    query = 'insert into ROAD_CONDITION values (ROAD_CONDITION_SEQ.NEXTVAL, :lat, :lon, :heading, :link_id, :visibility, :snow, :road_temp, :water_film, :friction, :code, :current_time)'
+
+
+    for i in range(len(RoadCondition_df)):
+        lat = int(RoadCondition_df.iloc[i]["latitude"])
+        lon = int(RoadCondition_df.iloc[i]["longitude"])
+        heading = int(RoadCondition_df.iloc[i]["heading"])
+        link_id = int(RoadCondition_df.iloc[i]["link_id"])
+        visibility = int(RoadCondition_df.iloc[i]["visibility"])
+        snow = int(RoadCondition_df.iloc[i]["snow"])
+        road_temp = int(RoadCondition_df.iloc[i]["road_temp"])
+        water_film = int(RoadCondition_df.iloc[i]["water_film"])
+        friction = int(RoadCondition_df.iloc[i]["friction"])
+        code = int(RoadCondition_df.iloc[i]["code"])
+
+        cur.execute(query, (lat, lon, heading, link_id, visibility, snow, road_temp, water_film, friction, code, current_time))
 
     con.commit()
     cur.close()
@@ -167,10 +267,10 @@ def wave_df_to_oracle(wave_df):
     cur.close()
     con.close()
 
-
 def print_main_menu():
-    print('\n1. 태풍 시뮬레이션 데이터 구축')
-    print('2. 스케줄러 종료')
+    print('\n1. 화창한날 시뮬레이션 데이터 구축')
+    print('2. 태풍 시뮬레이션 데이터 구축')
+    print('3. 스케줄러 종료')
     print('* 엔터: 메뉴 업데이트\n')
 
 
@@ -182,12 +282,18 @@ while True:
     else:                menu_num = int(selection)
 
     if(menu_num == 1):
-        hericane_ROADEVENT_to_oracle(make_ROADEVENT_df())
-        hericane_RoadWork_to_oracle(make_RoadWork_df())
-        hericane_RoadCondition_to_oracle(make_RoadCondition_df())
-        hericane_RoadClose_to_oracle(make_RoadClose_df())
-        #wave_df_to_oracle(make_wave_df())
+        sunny_ROADEVENT_to_oracle(make_sunny_df('ROADEVENT'))
+        sunny_RoadWork_to_oracle(make_sunny_df('RoadWork'))
+        sunny_RoadCondition_to_oracle(make_sunny_df('RoadCondition'))
+        sunny_RoadClose_to_oracl(make_sunny_df('RoadClose'))
+
     elif(menu_num == 2):
+        hericane_ROADEVENT_to_oracle(make_herricane_df('ROADEVENT'))
+        hericane_RoadWork_to_oracle(make_herricane_df('RoadWork'))
+        hericane_RoadCondition_to_oracle(make_herricane_df('RoadCondition'))
+        hericane_RoadClose_to_oracle(make_herricane_df('RoadClose'))
+
+    elif (menu_num == 3):
         break
     elif (menu_num == 0):
         continue
